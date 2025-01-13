ade4cbc469d9fed607d8afdf837ed83f5e58f7a7
Ali Tariq <ali.tariq@afiniti.com>
2025-01-13 15:48:50 +0500

    added urls

--- finance_management_system/urls.py
+++ finance_management_system/urls.py
@@ -5,7 +5,11 @@ from api import views
 router = DefaultRouter()
 router.register(r'transactions', views.TransactionViewSet, basename='transaction')
 router.register(r'budgets', views.BudgetViewSet, basename='budget')
-
+router.register(r'accounts', views.AccountViewSet, basename='account')
+router.register(r'recurring-expenses', views.RecurringExpenseViewSet, basename='recurring-expense')
+router.register(r'recurring-incomes', views.RecurringIncomeViewSet, basename='recurring-income')
+router.register(r'incomes', views.IncomeViewSet, basename='income')
+router.register(r'expenses', views.ExpenseViewSet, basename='expense')
 urlpatterns = [
     path('api/', include(router.urls))
 ]
