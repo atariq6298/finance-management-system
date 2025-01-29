718e969af24c99f4b4062305b94856a334b3e24a
Ali Tariq <atariq.sharafi@gmail.com>
2025-01-29 10:08:24 +0000

    changes made in settings.py

--- finance_management_system/settings.py
+++ finance_management_system/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'your-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['atariq6298.pythonanywhere.com']
 
 # Application definition
 
@@ -113,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.x/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.x/ref/settings/#default-auto-field
